95e78498872939c1f3cccb2f8db15a0878c8b0a0
tgreer <UKTamo@gmail.com>
2018-06-02 22:07:25 +0100

    Update blame.py

--- blame.py
+++ blame.py
@@ -3,11 +3,13 @@
 from random import choice
 import sys
 if __name__ == '__main__':
+    print len(sys.argv)
     if len(sys.argv) >= 2:
         thing2blame = sys.argv[1:]
         newstr = ''.join(e for e in thing2blame if e.isalnum())
         print "DAMMIT, {0}!".format(newstr)
     else:
+	print "elseflag"
         people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the Computer. Do you secretly hate the Computer, citizen?']
         person = choice(people2blame)
         print "It was {0}".format(person)
