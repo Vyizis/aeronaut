9ed4f5ac90d371d5d31022f0d8b8071e94569614
tgreer <UKTamo@gmail.com>
2018-06-02 22:03:39 +0100

    Update blame.py

--- blame.py
+++ blame.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
         newstr = ''.join(e for e in thing2blame if e.isalnum())
         print "DAMMIT, {0}!".format(newstr)
     else:
-        people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the C$
+        people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the Computer. Do you secretly hate the Computer, citizen?']
         person = choice(people2blame)
         print "It was {0}".format(person)
