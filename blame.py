94add516b5d6447d1ff7ae18bc0deee538dcaf63
tgreer <UKTamo@gmail.com>
2018-06-02 21:48:56 +0100

    Update blame.py

--- blame.py
+++ blame.py
@@ -6,7 +6,8 @@ import re
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         thing2blame = sys.argv[1:]
-        print "DAMMIT, {0}!".format(re.sub('\W+','', thing2blame))
+        blah = re.sub('\W+','', thing2blame)
+        print "DAMMIT, {0}!".format(blah)
     else:
         people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the Computer. Do you secretly hate the Computer, citizen?']
         person = choice(people2blame)
