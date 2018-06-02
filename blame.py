cdffd56f32619f8c4841ee95364d0b8fee2e7e06
tgreer <UKTamo@gmail.com>
2018-06-02 21:56:40 +0100

    Update blame.py

--- blame.py
+++ blame.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         thing2blame = sys.argv[1:]
-        newstr = thing2blame.join(e for e in string if e.isalnum())
+        newstr = ''.join(e for e in thing2blame if e.isalnum())
         print "DAMMIT, {0}!".format(newstr)
     else:
         people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the Computer. Do you secretly hate the Computer, citizen?']
