c8c1fa4fdbaa79fdfed0afb31d1e3971565492de
Joe Poser <jposer@jposer-vm.ad.genesis.global>
2018-09-27 11:40:57 +0100

    downgraded marvin

--- marvin.py
+++ marvin.py
@@ -1,3 +1,4 @@
-from random import randint
-quotes =['"I have a million ideas. They all point to certain death."', '"Pardon me for breathing, which I never do anyway so I don’t know why I bother to say it, oh God, I’m so depressed."', '"Life! Don’t talk to me about life."', '"I could calculate your chance of survival, but you won’t like it."', '"My capacity for happiness, you could fit into a matchbox without taking out the matches first"', '"Funny, how just when you think life can’t possibly get any worse it suddenly does."', '"I’d give you advice, but you wouldn’t listen. No one ever does."', '"I’ve seen it. It’s rubbish."', '"Do you want me to sit in a corner and rust, or just fall apart where I’m standing?"']
-print(quotes[randint(0,8)])
+from random import choice 
+quotes =['"I have a million ideas. They all point to certain death."', '"Pardon me for breathing, which I never do anyway so I don\'t know why I bother to say it, oh God, I\'m so depressed."', '"Life! Don\'t talk to me about life."', '"I could calculate your chance of survival, but you won\'t like it."', '"My capacity for happiness, you could fit into a matchbox without taking out the matches first"', '"Funny, how just when you think life can\'t possibly get any worse it suddenly does."', '"I\'d give you advice, but you wouldn\'t listen. No one ever does."', '"I\'ve seen it. It\'s rubbish."', '"Do you want me to sit in a corner and rust, or just fall apart where I\'m standing?"']
+
+print choice(quotes)
