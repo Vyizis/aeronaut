03120bfad1e0e2ee326d90d53a9f61d7dcbba564
Vyizis <vyizis@gmail.com>
2021-02-17 16:02:37 +0000

    update blame

--- scripts/blame.py
+++ scripts/blame.py
@@ -2,13 +2,11 @@
 
 from random import choice
 import os
-if __name__ == '__main__':
-    arg = os.environ.get('IRCCAT_ARGS')
-    thing2blame = ''.join(e for e in arg if e.isalnum())
-    if len(thing2blame) > 0:
-        print ("DAMMIT, {0}!".format(thing2blame))
+
+thing2blame = os.environ.get('IRCCAT_ARGS')
+if len(thing2blame) > 0:
+    print ("DAMMIT, {0}!".format(thing2blame))
     else:
         people2blame = ['nobody\'s fault, shit happens', 'vyizis', 'cptprime', 'cahbtexhuk', 'M0XIN', 'tgreer', 'the Computer. Do you secretly hate the Computer, citizen?']
         person = choice(people2blame)
-        # person = 'tgreer'
         print ("It was {0}".format(person))
