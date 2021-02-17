aca4f8904896e56d6f0eddf81d718d239eb3311f
Vyizis <vyizis@gmail.com>
2021-02-17 15:53:15 +0000

    python3

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 import os
-print "This is the name of the script: ", os.environ.get('IRCCAT_COMMAND')
-print "Number of arguments: ", len(os.environ.get('IRCCAT_ARGS'))
-print "The arguments are: " , str(os.environ.get('IRCCAT_ARGS'))
+print ("This is the name of the script: ", os.environ.get('IRCCAT_COMMAND')))
+print ("Number of arguments: ", len(os.environ.get('IRCCAT_ARGS')))
+print ("The arguments are: " , str(os.environ.get('IRCCAT_ARGS')))
