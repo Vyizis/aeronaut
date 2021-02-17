73baddbc9bbdea8a4c23aa3bce7567992c9c0f92
Dan Reid <Vyizis@users.noreply.github.com>
2021-02-17 15:13:08 +0000

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -3,4 +3,4 @@
 import os
 print "This is the name of the script: ", os.environ.get('IRCCAT_COMMAND')
 print "Number of arguments: ", len(os.environ.get('IRCCAT_ARGS'))
-print "The arguments are: " , str(args)
+print "The arguments are: " , str(os.environ.get('IRCCAT_ARGS'))
