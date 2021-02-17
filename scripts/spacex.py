cc09eba0c130ae2bf7fad83a3550fc51a45546c7
cptprime <chomiliert@me.com>
2021-02-17 22:59:31 +0000

    Update spacex.py
    not showing up the hour if the launch window isnt known

--- scripts/spacex.py
+++ scripts/spacex.py
@@ -2,14 +2,24 @@
 import requests
 import json
 import datetime
-response = requests.get('https://api.spacexdata.com/v4/launches/next')
+
+
+response = requests.get("https://api.spacexdata.com/v4/launches/next")
 
 data = json.loads(response.content)
 
-Mission_Name=data["name"]
+mission_name=data["name"]
 date_utc=data["date_utc"]
-date_time_obj = datetime.datetime.strptime(date_utc, '%Y-%m-%dT%H:%M:%S.%fZ')
-date_formatted=date_time_obj.strftime("%c")
-msg= "The next SpaceX launch is " + Mission_Name + " at the following date: " + date_formatted
+date_precision=data["date_precision"]
+
+date_time_obj = datetime.datetime.strptime(date_utc, "%Y-%m-%dT%H:%M:%S.%fZ")
+date_formatted=date_time_obj.strftime("%A the %d")
+hour_formatted=date_time_obj.strftime("%H %M %p")
+
+
+if "hour" in date_precision:
+	msg= f"The next launch is {mission_name} on {date_formatted} at {hour_formatted}"
+else:
+	msg= f"The next launch is {mission_name} on {date_formatted}"
 
 print(msg)
